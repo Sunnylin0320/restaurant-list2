056d484d941840da5013364f33ba758089f23494
Sunnylin0320 <sunny8503201@gmail.com>
2023-08-20 14:47:12 +0800

    feat:add bootstrap and popper

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+app.use(express.static("public"));
 // routes setting
 app.get("/", (req, res) => {
   res.render("index");
