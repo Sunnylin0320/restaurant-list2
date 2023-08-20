489f30d3e654ae5d8d46e3e3eafa19ea2ffabff4
Sunnylin0320 <sunny8503201@gmail.com>
2023-08-20 22:27:05 +0800

    feat: 載入json file

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const exphbs = require("express-handlebars");
+const restaurantList = require("./restaurant.json");
 
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -10,7 +11,8 @@ app.set("view engine", "handlebars");
 app.use(express.static("public"));
 // routes setting
 app.get("/", (req, res) => {
-  res.render("index");
+  // pass the movie data into 'index' partial template
+  res.render("index", { restaurants: restaurantList.results });
 });
 
 // start and listen on the Express server
