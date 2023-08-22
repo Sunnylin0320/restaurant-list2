b873b7a643aa9e446c95db1af45e27e4e54693de
Sunnylin0320 <sunny8503201@gmail.com>
2023-08-22 13:25:06 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,48 +1,18 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const mongoose = require("mongoose");
+const app = express();
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override"); 
 const routes = require("./routes");
-const app = express();
-
-
-
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 const port = 3000;
 
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
-
+require("./config/mongoose");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 app.use(express.static("public"));
 app.use(routes);
-
-// routes setting
-
-
-
-
 
 // start and listen on the Express server
 app.listen(port, () => {
---
+++ config/mongoose.js
@@ -0,0 +1,23 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const mongoose = require("mongoose");
+
+mongoose.set("useFindAndModify", false);
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", () => {
+  console.log("MongoDB is not connected.");
+});
+
+db.once("open", () => {
+  console.log("MongoDB is connected!");
+});
+
+module.exports = db;
