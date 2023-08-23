1d0637a1e400bb86163ef54180e1f1ec1b688a14
Sunnylin0320 <sunny8503201@gmail.com>
2023-08-23 13:35:06 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const session = require("express-session");
 const exphbs = require("express-handlebars");
 const app = express();
 const bodyParser = require("body-parser");
@@ -11,6 +12,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+app.use(
+  session({
+    secret: "ThisIsMySecret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 app.use(express.static("public"));
 app.use(routes);
 
